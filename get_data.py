ea2da05e972e2002d3ee038cac087700a3b754e6
Karolina Chalupová <karolina.chalupova@pravda.eu>
2020-06-21 09:37:41 +0200

    Add code to filter and load data

---
+++ get_data.py
@@ -0,0 +1,44 @@
+"""
+Loads data to memory
+"""
+
+import pandas as pd
+
+class Anomalies():
+
+    def __init__(self, 
+            path_data='data/01_anomalies_filtered/data.pkl', 
+            path_meta='data/anomalies_meta.xlsx'):
+
+            self.data = pd.read_pickle(path_data)
+            self.data.reset_index(inplace=True)
+            self.meta = pd.read_excel('data/00_anomalies_unfiltered/anomalies_meta.xlsx', sheet_name=1)
+            
+            # check that meta corresponds one to one with signals 
+            assert set(self.meta.sc.tolist()).difference(set(self.data.columns.unique().tolist())) == set()
+            assert set(self.data.columns.tolist()).difference(set(self.meta.sc.unique().tolist())) == {'DTID', 'FTID', 'date', 'r'}    
+    
+    @property
+    def classification1(self):
+        classification1 = dict()
+        for c in self.meta['class'].unique().tolist(): 
+            classification1[c.lower()] = self.meta[self.meta['class'] == c].name.tolist()
+        return classification1
+            
+    @property
+    def classification2(self):
+        classification2 = dict()
+        for c in self.meta.class2.unique().tolist(): 
+            classification2[c.lower()] = self.meta[self.meta['class2'] == c].name.tolist()
+        return classification2
+            
+    @property 
+    def sc_to_name(self):
+        return dict(zip(self.meta.name_sc, self.meta.name))
+    
+    @property 
+    def name_to_sc(self):
+        return dict(zip(self.meta.name, self.meta.name_sc))
+        
+
+
