05310dbb60bffdc7e1ceffafa2fb9a92481d4c80
Karolina Chalupová <chalupova.karolina@gmail.com>
2020-10-05 18:56:48 +0200

    Add patience arg, change default args

--- model.py
+++ model.py
@@ -13,14 +13,15 @@ from netdata import NetData
 if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
-    parser.add_argument("--epochs", default=2, type=int, help="Number of epochs.")
+    parser.add_argument("--batch_size", default=10000, type=int, help="Batch size.")
+    parser.add_argument("--epochs", default=100, type=int, help="Number of epochs.")
     parser.add_argument("--ytrain", default=9, type=int, help="Number of years in train set.")
     parser.add_argument("--yvalid", default=6, type=int, help="Number of years in validation set.")
     parser.add_argument("--ytest", default=1, type=int, help="Number of years in test set.")
-    parser.add_argument("--hidden_layers", default="32,16,8,4,2", type=str, help='Number of neurons in hidden layers.')
+    parser.add_argument("--hidden_layers", default="32", type=str, help='Number of neurons in hidden layers.')
     parser.add_argument("--learning_rate", default=0.001, type=float, help="Initial learning rate.")
-    parser.add_argument("--l1", default=0.001, typ=float, help='L1 regularization term.')
+    parser.add_argument("--l1", default=0.001, type=float, help='L1 regularization term.')
+    parser.add_argument("--patience", default=5, type=int, help="Patience for early stopping.")
     parser.add_argument("--seed", default=42, type=int, help="Random seed.")
     parser.add_argument("--threads", default=2, type=int, help="Maximum number of threads to use.")
     parser.add_argument("--verbose", default=False, action="store_true", help="Verbose TF logging.")
@@ -42,6 +43,9 @@ if __name__ == "__main__":
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
         ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
+
+    network = Network(args)
+    network.train()
 
 class Network(): 
     def __init__(self, args):
@@ -79,6 +83,7 @@ class Network():
             validation_data=(nd.valid.data["features"], nd.valid.data["labels"]),
             callbacks=[tb_callback, early_stopping_callback]
         )
+        self.model.save(os.path.join(args.logdir, "model.h5"))
     
     def evaluate(self):
         nd = NetData(ytrain=args.ytrain, yvalid=args.yvalid, ytest=args.ytest)
