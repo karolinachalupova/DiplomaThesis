c935889c9daafe85b9420945bc65a32466181135
Karolina Chalupová <chalupova.karolina@gmail.com>
2020-11-16 20:42:13 +0100

    Move some code to utils.py

---
+++ utils.py
@@ -0,0 +1,49 @@
+"""
+Small miscalaneous tools.
+"""
+
+import os
+import shutil
+import numpy as np
+import pandas as pd
+
+def delete_unfinished_logdirs(logs):
+    logdirs = [os.path.join(logs, x) for x in os.listdir(logs)]
+    finished = [os.path.isfile(os.path.join(logdir, "args.pickle")) for logdir in logdirs]
+    unfinished_logdirs = [logdir for f, logdir in zip(finished, logdirs) if not f]
+    # ask for confirmation
+    if len(unfinished_logdirs) > 0:
+        answer = ""
+        while answer not in ["y", "n"]:
+            answer = input("OK to delete {} folders in {} [Y/N]? ".format(
+                len(unfinished_logdirs), logs)).lower()
+        if answer == "y":
+            print("Deleting...")
+            for logdir in unfinished_logdirs: 
+                try:
+                    shutil.rmtree(logdir)
+                except OSError as e:
+                    print("Error: %s : %s" % (logdir, e.strerror))
+            print("Finished.")
+        else:
+            print("Aborted. No deletion performed.")
+    else: 
+        print("There are no unfinished logdirs in {}.".format(logs))
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def get_orders(row):
+    array = row.values
+    temp = sorted(list(array))
+    res = np.array([temp.index(i) for i in array])
+    return pd.Series(res, row.index)
+
+def get_parts(row, nparts):
+    array = row.values
+    temp = sorted(list(array))
+    res = np.array([temp.index(i) for i in array])
+    labels = list(reversed((np.arange(nparts)+1)*(30/nparts)))
+    return pd.qcut(pd.Series(res, row.index).rank(method='first'),nparts,labels).astype(float)
