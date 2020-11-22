9c62c3dc5746238b01a4dcab3297ff9e49084c6c
Karolina Chalupová <chalupova.karolina@gmail.com>
2020-11-22 17:20:20 +0100

    Change main in results.py, fix path_generic

--- results.py
+++ results.py
@@ -8,7 +8,6 @@ import numpy as np
 import os
 import pickle
 import argparse
-import tensorflow as tf
 import pandas as pd
 
 from nets import Net, Nets
@@ -17,15 +16,14 @@ from data import Cleaned, Simulated
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ensemble", default=True, action="store_true", help="Use ensembles instead of individual models.")
+    parser.add_argument("--ensemble", default=False, action="store_true", help="Whether to use ensembles or of seed models.")
     parser.add_argument("--dataset", default="cleaned", type=str, help="Which dataset class from data.py to use")
     parser.add_argument('--calculate_on', default="test", type=str, help="Where to calculate interpretability measures. test, train or valid")
 
     args = parser.parse_args([] if "__file__" not in globals() else None)
 
-    ########################## MUST RUN AT ALL TIMES  ################################
      # Set paths for results and models, create if necessary 
-    path_generic =os.path.join("{}".format(args.dataset),"{}".format("ensembles" if args.ensemble else "individual"))
+    path_generic =os.path.join("{}".format(args.dataset),"{}".format("ensembles" if args.ensemble else "seeds"))
     path_results = os.path.join("results", path_generic)
     path_models = os.path.join("models", path_generic)
     if not os.path.exists(path_results):
@@ -46,15 +44,18 @@ if __name__ == "__main__":
     # Set datasets
     [net.set_dataset(dataset, ytest=1) for net in models.nets]
 
-    # Calculate local integrated gradients
-    for net in models.nets:
-        loc, glob = net.integrated_gradients(on=args.calculate_on)
-        loc.to_csv(os.path.join(path_models, net.folder_name, 'integrated_gradients_{}.csv'.format(args.calculate_on)))
-    
-    # Calculate backtest
-    for net in models.nets: 
-        backtest = net.backtest()
-        backtest.to_csv(os.path.join(path_models, net.folder_name, 'backtest.csv'))
+    if args.ensemble: 
+        # Calculate local integrated gradients
+        for net in models.nets:
+            loc, glob = net.integrated_gradients(on=args.calculate_on)
+            path = net.__repr__().split(": ")[1]
+            loc.to_csv(os.path.join(path_models, path, 'integrated_gradients_{}.csv'.format(args.calculate_on)))
+        
+        # Calculate backtest
+        for net in models.nets: 
+            backtest = net.backtest()
+            path = net.__repr__().split(": ")[1]
+            backtest.to_csv(os.path.join(path_models, path, 'backtest.csv'))
 
     # Calculate all other 
     models.dataframe.to_csv(os.path.join(path_results, 'args.csv'))
